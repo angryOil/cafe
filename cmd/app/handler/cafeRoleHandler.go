fdd084785e47e6e295f799fb698946c79760dbb4
JOY <103836369+angryOil@users.noreply.github.com>
2023-11-01 06:04:21 +0900

    feat/37 cafe-role 수정 , 삭제 기능 추가 close #37

--- cmd/app/handler/cafeRoleHandler.go
+++ cmd/app/handler/cafeRoleHandler.go
@@ -190,4 +190,5 @@ func (h CafeRoleHandler) delete(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
